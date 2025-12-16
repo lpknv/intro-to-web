f2c8f7a0f2f1c436f4c82753e7e48cde96acf500
lpknv <nikitalipkanov@outlook.com>
2025-12-16 15:47:03 +0100

    Bonus - Filter

--- Zootopia/animals_web_generator.py
+++ Zootopia/animals_web_generator.py
@@ -1,38 +1,102 @@
 import json
 
 
-def load_data(file_path):
-    """ Loads a JSON file """
-    with open(file_path, "r") as handle:
-        return json.load(handle)
+def skin_types():
+    types = []
+
+    for animal in get_animals_data():
+        characteristics = animal.get("characteristics")
+        if not characteristics:
+            continue
+
+        skin_type = characteristics.get("skin_type")
+        if not skin_type:
+            continue
+
+        if skin_type not in types:
+            types.append(skin_type)
+
+    return types
+
+
+def show_skin_types():
+    counter = 0
+    for skin_type in skin_types():
+        counter += 1
+        print(f"{counter}. {skin_type}")
+
+
+def animals_by_skin_type_serialized(skin_type):
+    result = ""
+
+    for animal in get_animals_data():
+        if animal["characteristics"].get("skin_type") == skin_type:
+            result += serialize_animal(animal)
+
+    return result
+
+
+def read_file(file_path, file_type):
+    with open(file_path, "r", encoding="utf-8") as handle:
+        if file_type == "json":
+            return json.load(handle)
+        else:
+            return handle.read()
+
+
+def write_file(file_path, data):
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(data)
+
 
 def serialize_animal(animal_obj):
-    """ Serializes an animal object """
-    output = ""
-    output += f"""
+    html_output = ""
+    html_output += f"""
                 <li class="cards__item">
                     <div class="card__title">{animal_obj['name']}</div>
-                    <p class="card__text">
-                        <strong>Diet:</strong> {animal_obj['characteristics']['diet']}<br/>
-                        <strong>Location:</strong> {", ".join(animal_obj['locations'])}<br/>
-                        {('<strong>Type:</strong> ' + animal_obj['characteristics']['type']) if animal_obj['characteristics'].get('type') else ''}
-                    </p>
+                    <div class="card__text">
+                        <ul>
+                            <li><strong>Diet:</strong> {animal_obj['characteristics']['diet']}</li>
+                            <li><strong>Skin Type:</strong> {animal_obj['characteristics']['skin_type']}</li>
+                            <li><strong>Location:</strong> {", ".join(animal_obj['locations'])}</li>
+                            <li><strong>Lifespan:</strong> {animal_obj['characteristics']['lifespan']}</li>
+                            {('<li><strong>Type:</strong> ' + animal_obj['characteristics']['type'] + '</li>') if animal_obj['characteristics'].get('type') else ''}
+                        </ul>
+                    </div>
                 </li>
         """
-    return output
+    return html_output
 
 
-animals_data = load_data('animals_data.json')
+def get_animals_data():
+    return read_file("animals_data.json", 'json')
 
-output = ""
 
-for animal in animals_data:
-    output += serialize_animal(animal)
+def output_animal_html_file(file_template, output_path, data_to_replace):
+    html_content = read_file(file_template, None)
+    for key, val in data_to_replace.items():
+        html_content = html_content.replace(key, val)
+    write_file(output_path, html_content)
 
-with open("animals_template.html", "r", encoding="utf-8") as file:
-    html_content = file.read()
 
-html_content = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
+def main():
+    show_skin_types()
 
-with open("animals.html", "w", encoding="utf-8") as file:
-    file.write(html_content)
+    skin_type = input(f"What skin type do you want to use? ")
+    if skin_type not in skin_types():
+        print("Skin type not found. Try again...")
+    else:
+        output_animal_html_file(
+            "animals_template.html",
+            "animals.html",
+            {
+                "__REPLACE_SUBTITLE_FILTERED_BY_SKIN_TYPE__": f"<h2>Animals filtered by skin type: {skin_type}</h2>",
+                "__REPLACE_ANIMALS_INFO__": animals_by_skin_type_serialized(
+                    skin_type
+                )
+            }
+        )
+
+
+if __name__ == "__main__":
+    main()
