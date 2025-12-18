8adb1b1ef486805acf2d765b605a2c7be888f715
lpknv <nikitalipkanov@outlook.com>
2025-12-18 11:12:54 +0100

    Add doc strings to every function

--- Zootopia/animals_web_generator.py
+++ Zootopia/animals_web_generator.py
@@ -2,11 +2,13 @@ import json
 
 
 def get_animals_data():
+    """Read animals.json using json library and return animals data"""
     with open("animals_data.json", "r", encoding="utf-8") as file:
         return json.load(file)
 
 
 def skin_types(_animals):
+    """Store skin types in new list from original animals list"""
     types = []
 
     for animal in _animals:
@@ -18,11 +20,13 @@ def skin_types(_animals):
 
 
 def show_skin_types(_animals):
+    """Print available skin types"""
     for i, skin_type in enumerate(skin_types(_animals), start=1):
         print(f"{i}. {skin_type}")
 
 
 def animals_by_skin_type_serialized(_animals, skin_type):
+    """Filter animals by skin type and serialize them into an HTML string"""
     result = ""
 
     for animal in _animals:
@@ -34,6 +38,7 @@ def animals_by_skin_type_serialized(_animals, skin_type):
 
 
 def add_card_text_item(title, characteristics, key):
+    """Helper function to render a list item (HTML <li> tag)"""
     value = characteristics.get(key)
     if not value:
         return ""
@@ -43,6 +48,7 @@ def add_card_text_item(title, characteristics, key):
 
 
 def serialize_animal(animal):
+    """Serialize an animal into an HTML string"""
     characteristics = animal["characteristics"]
 
     return f"""
@@ -62,6 +68,11 @@ def serialize_animal(animal):
 
 
 def output_animal_html_file(template_path, output_path, replacements):
+    """
+    1. Read the HTML template file
+    2. Iterate through a dictionary to replace certain strings in the template file with the correct data
+    3. Write the HTML output file
+    """
     with open(template_path, "r", encoding="utf-8") as file:
         html = file.read()
 
@@ -73,26 +84,25 @@ def output_animal_html_file(template_path, output_path, replacements):
 
 
 def main():
-    # get animals data from json file
+    """
+    Load animal data from a JSON file, extract unique skin types,
+    filter animals based on a user-provided skin type (case-insensitive),
+    and generate an HTML file displaying the filtered results.
+    """
     animals = get_animals_data()
 
-    # show available skin types from all animals (without duplicates)
     show_skin_types(animals)
 
-    # read the skin type from user input and transform it to lower case
     skin_type_input = input("What skin type do you want to use? ")
 
-    # compare the user input to available skin types in lower case
     if skin_type_input.lower() not in [t.lower() for t in skin_types(animals)]:
         print("Skin type not found. Try again...")
         return
 
-    # output html file with appropriate animals data
     output_animal_html_file(
         "animals_template.html",
         "animals.html",
         {
-            # although not really necessary, indicate in the HTML that animals are filtered by skin type
             "__REPLACE_SUBTITLE_FILTERED_BY_SKIN_TYPE__":
                 f"<h2>Animals filtered by skin type: {skin_type_input}</h2>",
             "__REPLACE_ANIMALS_INFO__":
